1deed2397099a0af1985cbacf2a543ecf307cc7f
prabhatsih <prabhats1410@gmail.com>
2024-05-06 20:22:28 +0530

    Arrival Section first card complete

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// <!-- Hero Section Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// <!-- Hero Section Initialize Swiper -->
+
+// Animation Section Start //
+
+// Animation Section End //
